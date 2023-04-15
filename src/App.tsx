711f441d656793a89c3530a7ec5f7d588770fb5c
SaurabPoudel <poudelsaurab20@gmail.com>
2023-04-15 13:45:15 +0545

    Simple loader created later we will use another

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
 function App() {
-  return <>EngiMediQuest Control</>;
+  let i = 0;
+  while (i < 1000000000) {
+    i++;
+  }
+  return (
+    <>
+      <h1>EngiMediQuest</h1>
+    </>
+  );
 }
 
 export default App;
